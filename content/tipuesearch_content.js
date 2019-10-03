4e3f23428999356c340f56bd0bd44784b895227d
s40523124 <40523124@gm.nfu.edu.tw>
2019-10-03 14:21:11 +0800

    del

--- content/tipuesearch_content.js
+++
@@ -1,1 +0,0 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'http://mde.tw/cp2019 \xa0 \n https://dartpad.dev?id=984edab0aec4f456f5f5dd3a6da0bd9a  \xa0 \xa0 \xa0 \n \n \n \n', 'tags': '', 'url': 'About.html'}, {'title': 'USB', 'text': '', 'tags': '', 'url': 'USB.html'}, {'title': 'USB2', 'text': '', 'tags': '', 'url': 'USB2.html'}]};
